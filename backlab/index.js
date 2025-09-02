0a5de389ab523fa40aa850bc38689ac4daa92023
Hrushi1812 <hrushikesh1812@gmail.com>
2025-09-02 21:04:27 +0530

    linked frontend and backend

--- backlab/index.js
+++ backlab/index.js
@@ -29,9 +29,12 @@ const requestLogger = (request, response, next) => {
 
 app.use(express.json())
 app.use(requestLogger)
+
 const cors = require('cors')
 
 app.use(cors())
+
+app.use(express.static('dist'))
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
