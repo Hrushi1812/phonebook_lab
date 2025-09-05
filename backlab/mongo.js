ba4d65ec28f506ade3b0e264d199c164c5fc2695
Hrushi1812 <hrushikesh1812@gmail.com>
2025-09-05 14:08:12 +0530

    many changes lol!!

--- backlab/mongo.js
+++ backlab/mongo.js
@@ -7,7 +7,7 @@ if (process.argv.length < 3) {
 
 const password = process.argv[2]
 
-const url = `mongodb+srv://Hrushi18:${password}@cluster0.dccgegh.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
+const url = `mongodb+srv://Hrushi18:${password}@version1.lk9kr9m.mongodb.net/notesApp?retryWrites=true&w=majority&appName=Version1`
 
 mongoose.set('strictQuery',false)
 
@@ -20,20 +20,12 @@ const noteSchema = new mongoose.Schema({
 
 const Note = mongoose.model('Note', noteSchema)
 
-// const note = new Note({
-//   content: 'HTML is easy',
-//   important: true,
-// })
+const note = new Note({
+  content: 'HTML is easy',
+  important: true,
+})
 
-
-// note.save().then(result => {
-//   console.log('note saved!')
-//   mongoose.connection.close()
-// })  
-
-Note.find({}).then(result => {
-  result.forEach(note => {
-    console.log(note)
-  })
+note.save().then(result => {
+  console.log('note saved!')
   mongoose.connection.close()
 })
